0b887b28d19c7beace3f0710e612d1402ffa72d8
Mishiko Aspanidze <120841642+MishSoft@users.noreply.github.com>
2023-12-29 23:27:29 +0400

    Add files via upload

---
+++ api.js
@@ -0,0 +1,4 @@
+const api = 'BUqwUWUA1T1mPecvjy1k5hPN'
+
+
+export default api
--- script.js
+++ script.js
@@ -1,64 +1,69 @@
+import api from "./api.js";
+
 let imageURL;
 let imgElement;
 
-    function submitHandler(){
-        console.log("click");
-        const fileInput = document.getElementById('fileInput');
-        console.log(fileInput.files);
-        const image = fileInput.files[0];
+const uploadButton = document.getElementById("uploadBtn");
+const downloadButton = document.getElementById("downloadBtn");
 
-        // Multipart file
-        const formData = new FormData();
-        formData.append('image_file', image);
-        formData.append('size', 'auto');
+uploadButton.addEventListener("click", submitHandler);
+downloadButton.addEventListener("click", downloadFile);
 
-        const apiKey = 'BUqwUWUA1T1mPecvjy1k5hPN';
+function submitHandler() {
+  // console.log("click");
+  const fileInput = document.getElementById("fileInput");
+  // console.log(fileInput.files);
+  const image = fileInput.files[0];
 
-        fetch('https://api.remove.bg/v1.0/removebg',{
-            method:'POST',
-            headers: {
-            'X-Api-Key': apiKey
-         },
-         body: formData
-        })
-        .then(function(reponse){
-                return reponse.blob()
-        })
-        .then(function(blob){
-                let result = document.querySelector(".result")
-                const url = URL.createObjectURL(blob);
-                imageURL = url;
-                
-                if(imgElement) {
-                    result.removeChild(imgElement)
-                }
+  // Multipart file
+  const formData = new FormData();
+  formData.append("image_file", image);
+  formData.append("size", "auto");
 
-                imgElement = document.createElement('img');
-                imgElement.src = url;
-                result.appendChild(imgElement);
+  fetch("https://api.remove.bg/v1.0/removebg", {
+    method: "POST",
+    headers: {
+      "X-Api-Key": api,
+    },
+    body: formData,
+  })
+    .then(function (reponse) {
+      return reponse.blob();
+    })
+    .then(function (blob) {
+      let result = document.querySelector(".result");
+      const url = URL.createObjectURL(blob);
+      imageURL = url;
 
-        })
-        .catch(error => {
-            console.error("Error:", error)
-        });
+      if (imgElement) {
+        result.removeChild(imgElement);
+      }
+
+      imgElement = document.createElement("img");
+      imgElement.src = url;
+      result.appendChild(imgElement);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function downloadFile() {
+  if (imageURL) {
+    var anchorElement = document.createElement("a"); //<a></a>
+    anchorElement.href = imageURL;
+    anchorElement.download = "test.png";
+    document.body.appendChild(anchorElement);
+
+    anchorElement.click();
+
+    document.body.removeChild(anchorElement);
+
+    // Remove the image from the result div
+    if (imgElement) {
+      let result = document.querySelector(".result");
+      result.removeChild(imgElement);
+      imgElement = null;
     }
-
-   function downloadFile(){
-    if(imageURL) {
-        var anchorElement = document.createElement('a'); //<a></a>
-        anchorElement.href = imageURL;
-        anchorElement.download = 'test.png';
-        document.body.appendChild(anchorElement);
-
-        anchorElement.click();
-
-        document.body.removeChild(anchorElement);
-
-        // Remove the image from the result div
-        if (imgElement) {
-            let result = document.querySelector(".result");
-            result.removeChild(imgElement);
-            imgElement = null;
-        }
-    }
-   }
+  }
+}
